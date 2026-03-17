d010c9bfda31abb3dc6afd245fec08ede14e90fb
NCHAKRADHAR-SINGH1 <chakri.august2006@gmail.com>
2026-03-17 19:22:10 +0530

    Delete civic-voice directory

--- civic-voice/server/src/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import app from "./server.js";
-
-dotenv.config();
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Civic Voice API running on port ${port}`);
-});
